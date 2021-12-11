99e8898478f29fb0bb4f8b5b5253312d67013656
DARK WEBLOAD <93924598+DARK-WEBLOAD@users.noreply.github.com>
2021-12-11 10:54:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -33,8 +33,7 @@ class script(object):
 1. Bot should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
-
-SONG_TXT ="""<b>SONG MODULE</b>
+     SONG_TXT = """<b>SONG MODULE</b>
 
 Song Download
 Song Download Module, For Those Who Love Music
